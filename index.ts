bc960a5be4a748e7fd3085a16493372e6a730663
Kamil <kamgre7@gmail.com>
2023-09-06 19:55:57 +0200

    Added error handler

--- index.ts
+++ index.ts
@@ -3,12 +3,15 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import { appConfig } from './src/config/appConfig';
 import { animalsRouter } from './src/routes/animalsRouter';
+import { errorHandler } from './src/middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/animals', animalsRouter);
+
+app.use(errorHandler);
 
 app.listen(appConfig.port, appConfig.hostName, () => {
   console.log(`Application is running on ${appConfig.host}:${appConfig.port}`);
