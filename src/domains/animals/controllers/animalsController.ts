2395fcfd1d0cd9cc32afe75e2519e4e41dcf01fe
Kamil <kamgre7@gmail.com>
2023-09-20 13:41:36 +0200

    Added methods to controller

--- src/domains/animals/controllers/animalsController.ts
+++ src/domains/animals/controllers/animalsController.ts
@@ -5,11 +5,29 @@ import { Request, Response } from 'express';
 import { TYPES } from '../../types/types';
 import { ParsedRequest } from '../../../apiTypes';
 import { GetOneAnimalReq } from '../schemas/getOneSchema';
+import { UpdateByIdAnimalReq } from '../schemas/updateByIdSchema';
+import {
+  CreateAnimalReq,
+  CreateMultipleAnimalReq,
+} from '../schemas/createSchema';
+import { CreateAnimalsByTypeReq } from '../schemas/createByTypeSchema';
 
 export interface IAnimalsController {
+  create(req: ParsedRequest<CreateAnimalReq>, res: Response): Promise<void>;
+  createMultiple(
+    req: ParsedRequest<CreateMultipleAnimalReq>,
+    res: Response
+  ): Promise<void>;
+  createByType(
+    req: ParsedRequest<CreateAnimalsByTypeReq>,
+    res: Response
+  ): Promise<void>;
   getAll(req: Request, res: Response): Promise<void>;
-  getOne(req: Request, res: Response): Promise<void>;
-  updateByContext(req: Request, res: Response): Promise<void>;
+  getOne(req: ParsedRequest<GetOneAnimalReq>, res: Response): Promise<void>;
+  updateById(
+    req: ParsedRequest<UpdateByIdAnimalReq>,
+    res: Response
+  ): Promise<void>;
 }
 
 @injectable()
@@ -19,13 +37,51 @@ export class AnimalsController implements IAnimalsController {
     private readonly animalsService: IAnimalsService
   ) {}
 
-  async getAll(req: Request, res: Response): Promise<void> {
-    console.log('ADSADSAD');
+  async create(
+    req: ParsedRequest<CreateAnimalReq>,
+    res: Response
+  ): Promise<void> {
+    const data = req.body;
+    const animalId = await this.animalsService.create(data);
 
-    console.log(this.animalsService);
-    // const animals = await this.animalsService.getAll();
-    // console.log(animals);
-    res.status(200).json('animals');
+    res.status(201).json(animalId);
+  }
+
+  async createMultiple(
+    req: ParsedRequest<CreateMultipleAnimalReq>,
+    res: Response
+  ): Promise<void> {
+    const { animals } = req.body;
+    const createdAnimalsCounter = await this.animalsService.createMultiple(
+      animals
+    );
+
+    res.status(201).json({
+      createdAnimals: createdAnimalsCounter,
+    });
+  }
+
+  async createByType(
+    req: ParsedRequest<CreateAnimalsByTypeReq>,
+    res: Response
+  ): Promise<void> {
+    const { animals } = req.body;
+    const { type } = req.params;
+
+    const createdAnimalsCounter = await this.animalsService.createByType(
+      type,
+      animals
+    );
+
+    res.status(201).json({
+      createdAnimals: createdAnimalsCounter,
+    });
+  }
+
+  async getAll(req: Request, res: Response): Promise<void> {
+    const animals = await this.animalsService.getAll();
+
+    res.status(200).json(animals);
   }
 
   async getOne(
@@ -38,11 +94,15 @@ export class AnimalsController implements IAnimalsController {
     res.status(200).json(animal);
   }
 
-  async updateByContext(req: Request, res: Response): Promise<void> {
-    const id = req.params.id;
+  async updateById(
+    req: ParsedRequest<UpdateByIdAnimalReq>,
+    res: Response
+  ): Promise<void> {
+    const { id } = req.params;
     const data = req.body;
 
-    await this.animalsService.updateByContext(id, data);
-    res.status(200).send();
+    await this.animalsService.updateById(id, data);
+
+    res.status(204).end();
   }
 }
